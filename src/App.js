a07c06f4f24c103f4ce195c9d8c2efff5a369edf
Mark Abuyabor <mark@zeguestlist.com>
2022-08-07 11:45:10 +0800

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <div classNane="content">
-        <h1>App header</h1>
+      <Navbar></Navbar>
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
