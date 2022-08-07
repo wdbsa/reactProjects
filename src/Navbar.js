f564cde1a25ee2e8935c669036b34eeaa1ab6263
Mark Abuyabor <mark@zeguestlist.com>
2022-08-07 12:06:42 +0800

    add css styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Ninja Blog</h1>
+            <h1>Repteek Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
