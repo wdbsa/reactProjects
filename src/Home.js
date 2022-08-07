228008a489d9d56435a1e6a4ac851573b6a122fb
Mark Abuyabor <mark@zeguestlist.com>
2022-08-07 18:14:31 +0800

    handling click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('test', e);
+    };
+
+    const clickArg = (name, e) => {
+        console.log('name = ' + name, e.target);
+    };
+
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={ (e) => clickArg('Mark', e) }>Click this</button>
         </div>
 
      );
