d6fe7009773cc76e7f8a84f99f847739a5930d7c
Mark Abuyabor <mark@zeguestlist.com>
2022-08-08 10:53:53 +0800

    add lists

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState("mark");
-    const [age, setAge] = useState(30);
-   
-    const handleClick = () => {
-        setName("wdbsa");
-        setAge(78);
-    };
-
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mark', id: 3}
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <p>{ age }</p>
-            <button onClick={handleClick}>Click Me</button>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                </div>
+            ))}
         </div>
-
      );
 }
  
