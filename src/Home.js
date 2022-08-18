51973d95203204eb1b0ae13dc071ca385c0ee316
Mark Abuyabor <mark@zeguestlist.com>
2022-08-18 17:51:13 +0800

    catching error in react

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,31 @@ import BlogList from './blogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
        // console.log('use effect ran');
         fetch('http://localhost:8000/blogs')
             .then(response => {
+                if(!response.ok) {
+                    throw Error('could not fetched');
+                }
                return response.json();
             })
-            .then((data) => {
+            .then(data => {
                 setBlogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
                 setIsLoading(false);
             });
     }, []);
 
     return ( 
         <div className="home">
+        { error && <div>{ error }</div> }
         { isLoading && <div>Loading...</div> }
         {blogs && <BlogList blogs={blogs} title="The F blogs!" /> }
         </div>
