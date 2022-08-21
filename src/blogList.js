03aaf07e7352b5f8adb90964f5ab3d3a76330573
Mark Abuyabor <mark@zeguestlist.com>
2022-08-21 16:27:49 +0800

    route parameters

--- src/blogList.js
+++ src/blogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
 
     return (   
@@ -6,8 +8,10 @@ const BlogList = ({ blogs, title }) => {
 
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
