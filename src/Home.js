1366402bc9d5538636338469171165d9de90766b
Mark Abuyabor <mark@zeguestlist.com>
2022-08-21 15:27:38 +0800

    useEffect Cleanup

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import BlogList from './blogList';
 import useFetch from './useFetch';
 
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+        fetch(url, { signal: abortCont.signal })
             .then(response => {
                 if(!response.ok) {
                     throw Error('could not fetched');
@@ -19,10 +21,16 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
+                if(err.name === 'AbortError') {
+                    console.log('fetch aborted!!!');
+                } else {
+                    setError(err.message);
+                    setIsLoading(false);
+                }
             });
-    }, []);
+
+        return () => abortCont.abort();
+    }, [url]);
 
     return { data, isLoading, error }
 }
