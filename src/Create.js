8282aa7d15b67b642000e9ecd6c5e65dd7143174
Mark Abuyabor <mark@zeguestlist.com>
2022-08-22 09:52:50 +0800

    controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('wdbsa');
+
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+                required
+                >
+                </textarea>
+                <label>Blog author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="wdbsa">Wdbsa</option>
+                    <option value="drizzt">Drizzt</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
