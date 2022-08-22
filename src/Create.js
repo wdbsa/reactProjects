ef6b3de2cf1239c1e9cad374be9bf3bc8930be8c
Mark Abuyabor <mark@zeguestlist.com>
2022-08-22 10:22:43 +0800

    making post request

--- src/Create.js
+++ src/Create.js
@@ -4,13 +4,24 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('wdbsa');
+    const [isPending, setIsPending] = useState(false);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const blog = { title, body, author  };
+        const blog = { title, body, author };
 
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        });
     }
 
     return (
@@ -36,7 +47,8 @@ const Create = () => {
                     <option value="wdbsa">Wdbsa</option>
                     <option value="drizzt">Drizzt</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding Blog...</button> }
             </form>
         </div>
     );
